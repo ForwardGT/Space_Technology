0474e3c4eba471d501d654826a820cd0e6fda217
ForwardGT <ollmaincore@gmail.com>
2024-07-31 18:51:06 +0300

    добавлена вкладка AboutAppViewContent

--- app/src/main/java/com/example/spacetechnology/features/profile/presentation/ProfileScreen.kt
+++ app/src/main/java/com/example/spacetechnology/features/profile/presentation/ProfileScreen.kt
@@ -1,21 +1,51 @@
 package com.example.spacetechnology.features.profile.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.example.spacetechnology.core.uikit.navigation.SpaceTechNavigationBar
 import com.example.spacetechnology.core.uikit.theme.SpaceTechnologyTheme
+import com.example.spacetechnology.features.nasa.data.network.apiKeyNasa
+import com.example.spacetechnology.features.profile.presentation.view.AboutAppViewContent
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileScreen(
     navController: NavController
 ) {
+    val items = listOf("Profile", "Api key", "About")
+    var selectedItem by remember { mutableStateOf(items[0]) }
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
 
     Scaffold(
         bottomBar = {
@@ -23,15 +53,76 @@ fun ProfileScreen(
         }
     ) { paddingValues ->
         Box(Modifier.padding(paddingValues)) {
-            Text(text = "Profile")
+
+
+            ModalNavigationDrawer(
+                drawerState = drawerState,
+                drawerContent = {
+                    ModalDrawerSheet {
+                        Box(
+                            Modifier
+                                .size(30.dp)
+                                .background(Color.Red)
+                        ) {
+
+                        }
+                        items.forEach { item ->
+                            NavigationDrawerItem(
+                                label = { Text(item, fontSize = 22.sp) },
+                                selected = selectedItem == item,
+                                onClick = {
+                                    scope.launch { drawerState.close() }
+                                    selectedItem = item
+                                }
+                            )
+                        }
+                    }
+                },
+                content = {
+                    Column (Modifier.fillMaxSize()) {
+                        Box {
+                            IconButton(onClick = { scope.launch { drawerState.open() } },
+                                content = { Icon(Icons.Filled.Menu, null) }
+                            )
+                        }
+
+                        when (selectedItem) {
+                            "Profile" -> ProfileView()
+                            "Api key" -> ApikeyView()
+                            "About" -> AboutAppViewContent()
+                        }
+                    }
+                }
+            )
         }
     }
 }
 
+@Composable
+private fun ApikeyView() {
+    Column(
+
+        modifier = Modifier
+    ) {
+        Text(text = "Api key = $apiKeyNasa")
+    }
+}
+
+@Composable
+private fun ProfileView() {
+    Column(
+        modifier = Modifier
+
+    ) {
+        Text(text = "Hello")
+    }
+}
+
+
 
 @Preview
 @Composable
-fun Q() {
+private fun T() {
     SpaceTechnologyTheme(true) {
         ProfileScreen(navController = rememberNavController())
     }
---
+++ app/src/main/java/com/example/spacetechnology/features/profile/presentation/view/AboutAppViewContent.kt
@@ -0,0 +1,99 @@
+package com.example.spacetechnology.features.profile.presentation.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.spacetechnology.core.uikit.theme.SpaceTechColor
+import com.example.spacetechnology.core.uikit.theme.SpaceTechnologyTheme
+import com.example.spacetechnology.core.utils.CustomSpacer
+
+@Composable
+fun AboutAppViewContent() {
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+    ) {
+
+        MainHeadline(text = "SpaceX and NASA: Pioneering Technologies in Space Exploration")
+        SecondHeadline(text = "Introduction")
+        TextContent(text = "Space exploration has seen remarkable advancements thanks to the pioneering technologies developed by SpaceX and NASA. Both organizations have made significant contributions to our understanding of space and our ability to explore it, pushing the boundaries of what is possible.")
+        MainHeadline(text = "SpaceX Technologies")
+        SecondHeadline(text = "Reusable Rockets")
+        TextContent(text = "One of SpaceX's most groundbreaking innovations is the development of reusable rockets. The Falcon 9, for instance, is designed to return to Earth and be relaunched multiple times, significantly reducing the cost of space travel. This technology not only makes space missions more economical but also opens the door for more frequent and sustainable exploration.")
+        SecondHeadline(text = "Starship")
+        TextContent(text = "SpaceX's Starship is an advanced spacecraft designed for missions to Mars and beyond. With the capability to carry both crew and cargo, Starship represents a major leap forward in deep space exploration. It is designed to be fully reusable and aims to facilitate human settlement on other planets.")
+        SecondHeadline(text = "Dragon Spacecraft")
+        TextContent(text = "The Dragon spacecraft is another key innovation by SpaceX. It is capable of carrying humans to the International Space Station (ISS) and beyond. The Crew Dragon variant, in particular, has revolutionized crewed missions with its state-of-the-art safety features and advanced automation.")
+        MainHeadline(text = "NASA Technologies")
+        SecondHeadline(text = "Space Launch System (SLS)")
+        TextContent(text = "NASA's Space Launch System (SLS) is a powerful, heavy-lift rocket designed for deep space missions. It is the backbone of NASA's Artemis program, which aims to return humans to the Moon and eventually send astronauts to Mars. The SLS is designed to be flexible and evolvable, capable of carrying various payloads and crew.")
+        SecondHeadline(text = "Orion Spacecraft")
+        TextContent(text = "The Orion spacecraft is designed to carry astronauts farther into space than ever before. Equipped with advanced life support systems, navigation, and propulsion, Orion is central to NASA's plans for deep space exploration. It will work in conjunction with the SLS to facilitate human missions to the Moon, Mars, and beyond.")
+        SecondHeadline(text = "Mars Rovers")
+        TextContent(text = "NASA's Mars rovers, including the latest Perseverance rover, are equipped with cutting-edge technology to explore the Martian surface. These rovers are designed to conduct scientific experiments, search for signs of past life, and prepare for future human missions. They are equipped with advanced cameras, scientific instruments, and autonomous navigation systems.")
+        SecondHeadline(text = "Collaborative Efforts")
+        TextContent(text = "SpaceX and NASA often collaborate on missions, combining their expertise to achieve greater success. The Commercial Crew Program is a prime example, where SpaceX's Crew Dragon spacecraft is used to transport astronauts to the ISS, marking a new era of commercial spaceflight.")
+        SecondHeadline(text = "Conclusion")
+        TextContent(text = "SpaceX and NASA are at the forefront of space technology, each contributing uniquely to the advancement of space exploration. SpaceX's innovations in reusable rockets and spacecraft are making space more accessible and affordable, while NASA's development of powerful rockets and sophisticated spacecraft is paving the way for deep space missions. Together, these organizations are shaping the future of space travel, pushing the limits of human exploration and expanding our understanding of the universe.")
+
+    }
+
+}
+
+
+@Composable
+private fun MainHeadline(
+    text: String
+) {
+    Text(
+        modifier = Modifier.fillMaxWidth(),
+        textAlign = TextAlign.Center,
+        color = SpaceTechColor.white,
+        fontSize = 22.sp,
+        fontWeight = FontWeight.SemiBold,
+        text = text
+    )
+    CustomSpacer(v = 6.dp)
+}
+
+@Composable
+private fun SecondHeadline(
+    text: String
+) {
+    Text(
+        color = SpaceTechColor.white,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.SemiBold,
+        text = text
+    )
+    CustomSpacer(v = 1.dp)
+}
+
+@Composable
+private fun TextContent(
+    text: String
+) {
+    Text(
+        color = SpaceTechColor.white,
+        text = text
+    )
+    CustomSpacer(v = 6.dp)
+}
+
+
+@Preview
+@Composable
+private fun Z() {
+    SpaceTechnologyTheme(true) {
+        AboutAppViewContent()
+    }
+}
